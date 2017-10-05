748fdef3991301e53e961acd89885ad21c9aff45
Benjamin Nothdurft <b.nothdurft@gmail.com>
2017-10-05 08:32:49 +0200

    Add round 6.

--- db-migrations/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build -t dataduke/test-db-migrations .
-docker push dataduke/test-db-migrations
-
-ssh deploy@165.227.173.158 << EOF
-docker pull dataduke/test-db-migrations
-docker run --net web --rm dataduke/test-db-migrations
-docker rmi dataduke/sample-node
-EOF
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker build -t dataduke/sample-node .
+docker push dataduke/sample-node
+
+ssh deploy@165.227.173.15 << EOF
+docker pull dataduke/sample-node:latest
+docker stop web || true
+docker rm web || true
+docker rmi dataduke/sample-node:current || true
+docker tag dataduke/sample-node:latest dataduke/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 dataduke/sample-node:current
+EOF
